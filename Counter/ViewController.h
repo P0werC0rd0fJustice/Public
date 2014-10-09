295967fc490631503c1bde8f9a720d24d4717835
P0werC0rd0fJustice <tbreetz@gmail.com>
2014-10-09 00:20:24 -0400

    Added Counter App Files

---
+++ Counter/ViewController.h
@@ -0,0 +1,30 @@
+//
+//  ViewController.h
+//  Counter
+//
+//  Created by Tristan Breetz on 9/4/14.
+//  Copyright (c) 2014 Tristan Breetz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController{
+    IBOutlet UILabel *Label;
+    IBOutlet UIProgressView *Progress;
+    IBOutlet UISwitch *MultiplySwitch;
+    IBOutlet UIButton *AddOne;
+    IBOutlet UIButton *SubtractOne;
+    int Number;
+    float ProgressValue;
+}
+-(IBAction)addOne:(id)sender;
+-(IBAction)subtractOne:(id)sender;
+-(IBAction)reset:(id)sender;
+-(IBAction)multiplySwitch:(id)sender;
+-(IBAction)hiddenButton:(id)sender;
+
+
+
+
+@end
+
